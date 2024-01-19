367efdc2eff20e02c9e1416742f6e443ddbde45d
Anoop Kumar Tiwari <115396691+2411anoop@users.noreply.github.com>
2024-01-19 22:51:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from 'cors';
 import  path  from 'path';
+import {"fileURLToPath"} from 'url';
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule fix
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 
 // rest obj for api
 const app=express();
